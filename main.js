e4e44f9839b3f63a63c4637aa4fe4e4e9ac9403a
DavidCazalla <d_cazalla_b@hotmail.com>
2023-04-29 11:43:55 +0200

    Introducir numero aleatorio en un array sin que se repitan

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+let arrayHistory = []
+let numRandom
 
-
+//  Creacion de funcion para sacar numero aleatorio
 function getnumberRandom() {
     const min = 1;
     const max = 90;
     return min + Math.floor(Math.random() * max);
+}
+
+// Creacion de funcion para añadir en un array el numero aleatorio sin que se repita 
+function numberRandomUnique() {
+    numRandom = getnumberRandom(); 
+    if (!arrayHistory.includes(numRandom)) {
+      arrayHistory.push(numRandom);
+    } else {
+      numberRandomUnique(arrayHistory);
+    }
+    console.log(arrayHistory)
+    return numRandom;
 }
 
